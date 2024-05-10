cd09265ae782389ce7059ba3ec941635845aed10
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2024-05-10 10:09:49 -0300

    Punto 1 - Creación de objetos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Body } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+import { ProductEntity } from 'src/entities/product.entity';
 
 @Controller('products')
 export class ProductsController {
+    
+    constructor(private readonly productsService: ProductsService) {}
+
+        
+    @Post() //crear un objeto con @post
+    async createProduct(@Body() product: ProductEntity): Promise<ProductEntity> {
+        return await this.productsService.createProduct(product);
+    }
 
 
+
+    
 
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+import { ProductEntity } from 'src/entities/product.entity';
+import { HttpException } from '@nestjs/common';
+
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+    repository = ProductEntity
+
+    //Crear objetos con post
+    async createProduct(product: ProductEntity): Promise<ProductEntity> {
+        try {
+            //Podemos utilizar save porque la entidad ProductEntity
+            //extiende a BaseEntity
+            //Usamos await para esperar la ejecución del save
+            return await this.repository.save(product)
+        } catch {
+            throw new HttpException("Error creando producto", 500)
+        } finally {
+            console.log("Terminó la ejecución")
+        }
+    }
+
+}
