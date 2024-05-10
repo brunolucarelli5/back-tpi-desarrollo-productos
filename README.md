bf18262af25a935008b797c0baf85875e94451b8
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2024-05-10 09:57:09 -0300

    V1.3 - Base para empezar

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Liendo, Lucarelli, Monti, Mosconi
 Desarrollo - UTN FRVM
+
+COMANDOS A EJECUTAR
+
+npm i -g @nestjs/cli		// Instalar el cliente de NestJs, lo usamos para backend.
+nest new nombreProyecto	// Crear app. Elegir la opción npm.
+nest start --watch --open 	// Abrimos el servidor.
+
+npm install --save @nestjs/typeorm typeorm
+npm install sqlite3 --save
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forRoot({
+    //Usamos forRoot porque estamos inicializando la base de datos.
+    //Como es SQLITE es este formato, si fuera postgres sería otro
+    type: 'sqlite',
+    database: 'ProductsRepository.db',
+    entities: entities,
+    synchronize: true, //busca cambios en la DB cada que prendamos la app
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
